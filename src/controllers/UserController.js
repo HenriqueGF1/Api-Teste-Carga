1a869a6d5404d456ff5498c8212f3d036e5155bd
Henrique Gonçalves Ferreira <goncalves.f.henrique@gmail.com>
2021-06-24 20:28:30 -0300

    Fixed controller user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,11 +1,16 @@
 const UserModel = require('../models/UserModel');
 
 const listAllUsers = async (req, res) => {
+  let count = 0;
   try {
-    const usuarios = await UserModel.find({});
+    // const usuarios = await UserModel.find({});
+    // res.send({
+    //   message: 'Success',
+    //   data: usuarios,
+    // });
     res.send({
       message: 'Success',
-      data: usuarios,
+      data: `Usuário ${count + 1}`,
     });
   } catch (error) {
     res.send({
